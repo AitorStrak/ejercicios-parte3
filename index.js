d3dde6522f99e1fbd783412f94594ccc57f185f0
AitorStrak <121254558+AitorStrak@users.noreply.github.com>
2024-05-09 17:24:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,111 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-let notes = [
+let persons = [
   {
-    id: 1,
-    content: "HTML is easy",
-    important: true
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": "1"
   },
   {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": "2"
   },
   {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": "3"
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": "4"
+  },
+  {
+    "id": "5",
+    "name": "Aitor",
+    "number": "646937014"
   }
-]
+];
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
 
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use(requestLogger)
+app.use(express.json());
+app.use(requestLogger);
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'Endpoint desconocido' });
 }
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  response.send('<h1>Menú principal</h1>');
+});
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
     return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+      error: 'Contenido perdido' 
+    });
+  };
 
-  const note = {
+  const person = {
     content: body.content,
     important: body.important || false,
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  persons = persons.concat(person);
 
-  response.json(note)
+  response.json(person);
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
   } else {
-    console.log('x')
-    response.status(404).end()
-  }
-})
+    console.log('x');
+    response.status(404).end();
+  };
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Servidor funcionando en el puerto: ${PORT}`);
+});
